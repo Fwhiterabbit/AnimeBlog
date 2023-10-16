2fa2a2cb69a105ac69a4b9aced2f0c6966e4c6ae
Fwhiterabbit <thisismichael91@gmail.com>
2023-10-16 18:49:18 +0000

    deploy fix

--- myanimeblog/settings.py
+++ myanimeblog/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myanime-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['myanime-blog-4758927d3dc2.herokuapp.com', 'localhost']
 
 
 # Application definition
