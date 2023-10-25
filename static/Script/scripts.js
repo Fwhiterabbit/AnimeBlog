2b0c0217c62370a30ca39d8dbedcb0882a1a498f
Fwhiterabbit <thisismichael91@gmail.com>
2023-10-25 21:23:26 +0000

    added JS and style for logo

---
+++ static/Script/scripts.js
@@ -0,0 +1,8 @@
+function changeText(element) {
+    element.innerHTML = "アニメブログ";
+    element.style.color = "#ff69b4";
+    element.style.textShadow = "2px 2px 8px rgba(255, 105, 180, 0.5)";
+    element.style.fontFamily = "Arial, 'Kanit', sans-serif";
+    element.style.fontSize = "28px";
+}
+
